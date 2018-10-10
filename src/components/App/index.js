bb5d1d32d1b1d1c07ac0e8ab106b36f2c720d9d5
Amine Zaghdoudi <az@make.org>
2018-10-10 12:02:12 +0200

    fix path of related module into App :bug:

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from 'styled-components';
-import ModernNormalize from './assets/css-in-js/ModernNormalize';
-import FontFaces from './assets/css-in-js/FontFaces';
-import DefaultStyle from './assets/css-in-js/DefaultStyle';
-import { AppWrapper, MainContent } from './components/Styled/MainElements';
-import MainHeaderContainer from './containers/MainHeaderContainer';
-import MainFooterContainer from './containers/MainFooterContainer';
-import theme from './assets/vars/theme';
+import MainHeaderContainer from '../../containers/MainHeaderContainer';
+import MainFooterContainer from '../../containers/MainFooterContainer';
+import { AppWrapper, MainContent } from '../Styled/MainElements';
+import ModernNormalize from '../../assets/css-in-js/ModernNormalize';
+import FontFaces from '../../assets/css-in-js/FontFaces';
+import DefaultStyle from '../../assets/css-in-js/DefaultStyle';
+import theme from '../../assets/vars/theme';
 
 class App extends Component {
   render() {
