85a8f60e2160bcfb7b4de0ffe7dbde9431ed8976
Olivier Donze <od@make.org>
2018-10-11 18:05:37 +0200

    Adding pannel feature :sparkles:

--- src/components/ProposalSubmit/ProposalSubmitButtonComponent.js
+++ src/components/ProposalSubmit/ProposalSubmitButtonComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
-import { RedButton, GreyButton, IconInButton } from '../Styled/ButtonElements';
+import { RedButton, GreyButton, IconInButton } from '../Elements/ButtonElements';
 
 const ProposalSubmitButtonComponent = ({ canSubmit, handleSubmit }) => {
   if (canSubmit) {
