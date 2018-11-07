166e5160b9839628676c0035f467dbde63dec360
Amine Zaghdoudi <az@make.org>
2018-11-07 11:01:05 +0100

    remove api proxy

--- server/configuration.js
+++ server/configuration.js
@@ -1,6 +1,5 @@
 module.exports = {
   apiUrl: process.env.API_URL || 'https://api.preprod.makeorg.tech',
-  proxyApiUrl: [process.env.FRONT_URL || 'https://accessible.preprod.makeorg.tech', 'api'].join('/'),
   port: process.env.PORT || 9009,
   host: process.env.HOST || 'localhost'
 };
