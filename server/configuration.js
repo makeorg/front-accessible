de334af9f92d6b1cb197554850bbfcaf4bcb7bd4
Amine Zaghdoudi <az@make.org>
2018-12-19 16:51:50 +0100

    fix(api): use frontUrl for calling proxy api :beetle:
    close #54

--- server/configuration.js
+++ server/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
   apiUrl: process.env.API_URL || 'https://api.preprod.makeorg.tech',
-  frontUrl: process.env.FRONT_URL || 'https://www.preprod.makeorg.tech',
+  frontUrl: process.env.FRONT_URL || 'https://accessible.preprod.makeorg.tech',
   port: process.env.PORT || 9009,
   host: process.env.HOST || 'localhost'
 };
