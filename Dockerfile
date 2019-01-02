0b3365bfa7a16df32d090bc10a3c329c295401bd
Amine Zaghdoudi <az@make.org>
2019-01-02 12:03:43 +0100

    feat(transverse): add version file generation + route :sunflower:
    close #53

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM keymetrics/pm2:latest-alpine
 
+RUN apk --no-cache add curl git
+
 WORKDIR /usr/app/
 COPY package*.json ./
 COPY yarn.lock ./
 RUN yarn install -qy
 COPY . ./
 RUN yarn build
+RUN yarn version:build
 RUN yarn styleguidist build
 RUN yarn update:i18n
 
-RUN apk --no-cache add curl
 
 ENV PORT 8000
 ENV API_URL https://api.preprod.makeorg.tech
---
+++ server/versionBuild.js
@@ -0,0 +1,36 @@
+/**
+ * This script utility is needed to create version file.
+ */
+
+const fs = require('fs');
+const childProcess = require('child_process');
+const { BUILD_DIR } = require('./paths');
+
+const lastCommit = childProcess
+  .execSync('git rev-parse HEAD')
+  .toString().trim();
+const branch = childProcess
+  .execSync('git rev-parse --abbrev-ref HEAD')
+  .toString().trim();
+
+const projectName = 'make.org-frontend';
+const dateTime = new Date();
+
+const version = {
+  name: projectName,
+  version: lastCommit.substr(0, 10),
+  gitCommit: lastCommit,
+  gitBranch: branch,
+  buildTime: dateTime.toISOString()
+};
+
+try {
+  const versionPathFile = `${BUILD_DIR}/version`;
+
+  fs.writeFileSync(versionPathFile, JSON.stringify(version, null, 2), 'utf8');
+} catch (error) {
+  console.error(`error when writing version file => ${error}`);
+  return;
+}
+
+console.info('Version file created successfully!');
