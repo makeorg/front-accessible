17716a7166db8b1b2e199f0383031a57ac7f828f
Kimak <mickael.dumand@gmail.com>
2019-01-07 16:05:23 +0100

    feat(sequence): configure extra slides from query

---
+++ server/ssr/helpers/query.helper.js
@@ -0,0 +1,12 @@
+export function queryParamIsDisable(query, param) {
+  return query[param] === 'false';
+}
+
+export function disableExtraSlidesByQuery(sequenceExtraSlides, query) {
+  const extraSlidesConfig = { ...sequenceExtraSlides };
+  if (queryParamIsDisable(query, 'introCard')) delete extraSlidesConfig.introCard;
+  if (queryParamIsDisable(query, 'finalCard')) delete extraSlidesConfig.finalCard;
+  if (queryParamIsDisable(query, 'pushProposal')) delete extraSlidesConfig.pushProposal;
+  if (queryParamIsDisable(query, 'signUpCard')) delete extraSlidesConfig.signUpCard;
+  return extraSlidesConfig;
+}
--- server/ssr/sequenceRoute.js
+++ server/ssr/sequenceRoute.js
@@ -1,4 +1,6 @@
 import QuestionService from '../../src/api/QuestionService';
+import SequenceService from '../../src/api/SequenceService';
+import { disableExtraSlidesByQuery } from './helpers/query.helper';
 
 const reactRender = require('../reactRender');
 
@@ -6,14 +8,24 @@ async function getQuestion(questionSlug) {
   return QuestionService.getDetail(questionSlug);
 }
 
+async function getQuestionConfiguration(questionSlug, country) {
+  return SequenceService.fetchConfiguration(questionSlug, country);
+}
+
 module.exports = async function SequenceRoute(req, res) {
   try {
-    const question = await getQuestion(req.params.questionSlug);
+    const { questionSlug } = req.params;
+    const question = await getQuestion(questionSlug);
+    const questionConfiguration = await getQuestionConfiguration(questionSlug, question.country);
+    const { sequenceExtraSlides } = questionConfiguration;
+    questionConfiguration.sequenceExtraSlides = disableExtraSlidesByQuery(sequenceExtraSlides, req.query);
+
     const { firstProposal } = req.query;
 
     let sequenceState = {
       sequence: {
         question,
+        questionConfiguration,
         votedProposalIds: []
       }
     };
