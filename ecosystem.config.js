46bfe3e92758ef6009475becdba2d5f0e3f87fe9
Amine Zaghdoudi <az@make.org>
2019-01-08 12:13:42 +0100

    fix(transverse): remove env variable into pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,6 @@ module.exports = {
     name: 'front-accessible',
     script: 'bin/start',
     exec_mode: 'cluster',
-    instances: 8,
-    env: {
-      NODE_ENV: 'production'
-    }
+    instances: 8
   }]
 };
