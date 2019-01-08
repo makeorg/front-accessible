a8bd9400f11fa78048a427ff9a0402a6b8a7dffb
Amine Zaghdoudi <az@make.org>
2019-01-08 15:07:46 +0100

    fix(env): call internal api with relative path :cyclone:

--- src/api/SequenceService.js
+++ src/api/SequenceService.js
@@ -1,12 +1,6 @@
 /* eslint no-console: ["error", { allow: ["log"] }] */
 /* @flow */
 import axios from 'axios';
-import { FRONT_URL } from 'Constants/config';
-
-// todo: remmove it when question api is ready
-const PROXY_URL = (process.env.NODE_ENV === 'development')
-  ? 'http://localhost:9009'
-  : FRONT_URL;
 
 const PATH_QUESTION_CONFIGURATION = '/api/questions/:questionSlug?country=:country';
 
@@ -14,8 +8,11 @@ export default class SequenceService {
   static fetchConfiguration(questionSlug: string, country: string): Promose<Object> {
     return axios({
       method: 'GET',
-      url: `${PROXY_URL}${PATH_QUESTION_CONFIGURATION
-        .replace(':questionSlug', questionSlug).replace(':country', country)}`
+      url: `${PATH_QUESTION_CONFIGURATION
+        .replace(':questionSlug', questionSlug).replace(':country', country)}`,
+      proxy: {
+        port: process.env.PORT
+      }
     })
       .then(response => response.data)
       .catch(error => console.log(error));
