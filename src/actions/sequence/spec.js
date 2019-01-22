00fd84489eac3b905f85f72eb3ef50a0893864ab
Amine Zaghdoudi <az@make.org>
2019-01-22 15:50:58 +0100

    feat(test): add test for tracking first

--- src/actions/sequence/spec.js
+++ src/actions/sequence/spec.js
@@ -9,6 +9,7 @@ import MockAdapter from 'axios-mock-adapter';
 import * as actionTypes from 'Constants/actionTypes';
 import UserService from 'Api/UserService';
 import * as actions from './index';
+import Tracking from 'Services/Tracking';
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares);
@@ -17,15 +18,17 @@ const axiosMock = new MockAdapter(axios);
 
 describe('Sequence Actions', () => {
   let sandbox;
+  let trackingService;
   beforeEach(function () {
     sandbox = sinon.createSandbox();
+    trackingService = sandbox.mock(Tracking);
     store.clearActions();
     axiosMock.restore();
     axiosMock.onPost('/tracking/front').reply(204);
   });
 
   afterEach(function () {
-      sandbox.restore();
+    sandbox.restore();
   });
 
   it('Creates SEQUENCE_COLLAPSE when calling action', () => {
@@ -68,7 +71,7 @@ describe('Sequence Actions', () => {
     expect(store.getActions()).to.deep.equal(expectedActions)
   });
 
-  it('Test sequenceVote', () => {
+  it('Track sequence vote when first vote', () => {
     const store = mockStore({
       sequence: { votedProposalIds: [] }
     });
@@ -81,24 +84,43 @@ describe('Sequence Actions', () => {
       payload: { proposalId }
     }];
 
+    trackingService
+      .expects('trackVote')
+      .once()
+      .withArgs(proposalId, voteKey, index);
+
+    trackingService
+      .expects('trackFirstVote')
+      .once()
+      .withArgs(proposalId, voteKey, index);
+
     store.dispatch(actions.sequenceVote(proposalId, voteKey, index));
-    expect(store.getActions()).to.deep.equal(expectedActions)
+    expect(store.getActions()).to.deep.equal(expectedActions);
   });
 
-  it('Test sequenceUnvote', () => {
+  it('Track sequence vote when second vote', () => {
     const store = mockStore({
-      sequence: { votedProposalIds: [] }
+      sequence: { votedProposalIds: ['fooId'] }
     });
     const proposalId = 'foo';
     const voteKey = 'bar';
     const index = 0;
 
     const expectedActions = [{
-      type: actionTypes.SEQUENCE_PROPOSAL_UNVOTE,
+      type: actionTypes.SEQUENCE_PROPOSAL_VOTE,
       payload: { proposalId }
     }];
 
-    store.dispatch(actions.sequenceUnvote(proposalId, voteKey, index));
-    expect(store.getActions()).to.deep.equal(expectedActions)
+    trackingService
+      .expects('trackVote')
+      .once()
+      .withArgs(proposalId, voteKey, index);
+
+    trackingService
+      .expects('trackFirstVote')
+      .never();
+
+    store.dispatch(actions.sequenceVote(proposalId, voteKey, index));
+    expect(store.getActions()).to.deep.equal(expectedActions);
   });
 });
