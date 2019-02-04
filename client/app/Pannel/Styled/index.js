9666f41f4e2bd6987774864a0aeb16bbd71c12f6
Olivier Donze <od@make.org>
2019-02-04 16:54:32 +0100

    fix(sequence): sliding pannel padding bottom :bread:

--- client/app/Pannel/Styled/index.js
+++ client/app/Pannel/Styled/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { pxToRem } from 'Shared/helpers/styled';
 import { BasicColors, ShadowColors } from 'Client/app/assets/vars/Colors';
 import { Breakpoints, Layouts } from 'Client/app/assets/vars/Breakpoints';
+import { Elements } from 'Client/app/assets/vars/Elements';
 import CloseButton from './Button';
 
 const Pannel = styled.div`
@@ -12,7 +13,7 @@ const Pannel = styled.div`
   width: 100%;
   max-width: ${pxToRem(Layouts.ContainerWidth)};
   height: calc( 100% - ${pxToRem('15px')});
-  padding: ${pxToRem('50px')} ${pxToRem('20px')}  ${pxToRem('67px')}};
+  padding: ${pxToRem('50px')} ${pxToRem('20px')}  ${pxToRem(Elements.SequenceFooterHeightMobile)};
   box-shadow: 0 0 2px 0 ${ShadowColors.BlackZeroThreOpacity};
   background: ${BasicColors.PureWhite};
   background-color: ${BasicColors.PureWhite};
@@ -20,7 +21,7 @@ const Pannel = styled.div`
   transition: transform 0.5s linear;
   @media (min-width: ${pxToRem(Breakpoints.Desktop)}){
     height: calc( 100% - ${pxToRem('25px')});
-    padding: ${pxToRem('50px')} ${pxToRem('20px')} ${pxToRem('78px')}};
+    padding: ${pxToRem('50px')} ${pxToRem('20px')} ${pxToRem(Elements.SequenceFooterHeightDesktop)};
   }
 `;
 
