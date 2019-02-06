229c25612302168aec792c120ef327d04cf57af6
Philippe de Araujo <pa@make.org>
2019-02-06 09:36:30 +0100

    fix(tracking): refactor and fix twitter tracking
    closes #182

--- shared/services/Trackers/TwitterTracking.js
+++ shared/services/Trackers/TwitterTracking.js
@@ -1,8 +1,9 @@
 // @flow
+import Logger from 'Shared/services/Logger';
 import * as trackingConstants from 'Shared/constants/tracking';
 import { env } from 'Shared/env';
+import { twttr } from './twttr';
 
-let initialized = false;
 
 const twitterEventMapping = {
   [trackingConstants.DISPLAY_SEQUENCE]: 'o173q',
@@ -15,60 +16,31 @@ const twitterEventMapping = {
 };
 
 const isInitialized = (): boolean => {
-  if (!initialized) {
-    console.warn('Twitter Tracking not initialized before using call TwitterTracking.init with required params');
+  if (!twttr.initialized()) {
+    Logger.logWarning('Twitter Tracking not initialized');
   }
 
-  return initialized;
+  return twttr.initialized();
 };
 
 export default {
-  init(conversionId: string): void {
-    /* eslint-disable */
-    !(function (e, t, n, s, u, a) {
-      e.twq || (s = e.twq = function () {
-        // $FlowFixMe
-        s.exe ? s.exe(...arguments) : s.queue.push(arguments);
-      }, s.version = '1.1', s.queue = [], u = t.createElement(n), u.async = !0, u.src = '//static.ads-twitter.com/uwt.js',
-        // $FlowFixMe
-        a = t.getElementsByTagName(n)[0], a.parentNode.insertBefore(u, a));
-    }(window, document, 'script'));
-    /* eslint-enable */
-
-    if (conversionId) {
-      // $FlowFixMe
-      twq('init', conversionId); // eslint-disable-line no-undef
-      initialized = true;
-    } else {
-      console.error('TwitterConvTrkr.init(convId) missing conversion id.');
-    }
-  },
-
-  pageView(): void {
-    if (!isInitialized()) {
-      return;
-    }
-
-    // $FlowFixMe
-    twq('track', 'PageView'); // eslint-disable-line no-undef
-  },
-
   track(action: string): void {
     if (!isInitialized()) {
       return;
     }
 
     if (twitterEventMapping[action] === undefined) {
+      Logger.logError(`twitter action not found: ${action}`);
       return;
     }
 
     const eventName = twitterEventMapping[action];
 
     if (env.isDev()) {
-      console.info(`Tracking Twitter: event ${eventName}`);
+      Logger.logInfo(`Tracking Twitter: event ${eventName}`);
+      return;
     }
 
-    // $FlowFixMe
-    twq('track', eventName); // eslint-disable-line no-undef
+    twttr.track(eventName);
   }
 };
---
+++ shared/services/Trackers/twttr.js
@@ -0,0 +1,9 @@
+export const twttr = {
+  initialized() {
+    return window && window.twttr !== undefined;
+  },
+
+  track(eventName) {
+    return window.twttr.conversion.trackPid(eventName, { tw_sale_amount: 0, tw_order_quantity: 0 }); // eslint-disable-line no-undef
+  }
+};
