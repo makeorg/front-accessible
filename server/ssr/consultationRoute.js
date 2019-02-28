7a1f6b8642646cf0ded3416901c9f337d5f7d822
Olivier Donze <od@make.org>
2019-02-28 15:41:19 +0100

    feat(transverse): fetch question on consultation page :question:

--- server/ssr/consultationRoute.js
+++ server/ssr/consultationRoute.js
@@ -1,11 +1,33 @@
+import { QuestionService } from 'Shared/api/QuestionService';
+import { SequenceService } from 'Shared/api/SequenceService';
+import { createInitialState } from 'Shared/store/initialState';
 import { logError } from './helpers/ssr.helper';
 import { reactRender } from '../reactRender';
+
+async function getQuestion(questionSlug, headers) {
+  return QuestionService.getDetail(questionSlug, headers);
+}
+
+async function getQuestionConfiguration(questionSlug) {
+  return SequenceService.fetchConfiguration(questionSlug);
+}
 
 export const consultationRoute = async (req, res) => {
   let routeState = {};
 
   try {
-    routeState = {};
+    const initialState = createInitialState();
+    const { questionSlug } = req.params;
+    const question = await getQuestion(questionSlug);
+    const questionConfiguration = await getQuestionConfiguration(questionSlug);
+
+    routeState = {
+      consultation: {
+        ...initialState.consultation,
+        question,
+        questionConfiguration,
+      },
+    };
   } catch (error) {
     logError(error);
 
