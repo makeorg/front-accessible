2f21becf01ee915845a6450ca501e0e7920119bd
Kimak <mickael.dumand@gmail.com>
2019-03-06 17:21:17 +0100

    fix(transverse): add fonts to styleguide

--- styleguide.wrapper.js
+++ styleguide.wrapper.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from 'Shared/store';
+import { FontFacesStylesheet } from 'Client/app/assets/css-in-js/FontFaces';
 import { questionConfigurationFixture } from 'Shared/types/__fixtures__/sequence.fixture';
 import { ThemeProvider } from 'styled-components';
 import { ApiService } from 'Shared/api/ApiService';
@@ -16,6 +17,7 @@ ApiService.strategy = apiClient;
 /* eslint-disable import/no-default-export */
 export default ({ children }) => (
   <div>
+    <FontFacesStylesheet />
     <BrowserRouter>
       <Provider store={configureStore()}>
         <ThemeProvider theme={questionConfigurationFixture.theme}>
