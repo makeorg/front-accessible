5b138df9ffc97ee6a36a1ed3cb198fe89695362a
Olivier Donze <od@make.org>
2019-03-21 11:49:15 +0100

    feat(proposal): add year in proposal date displayed :calendar:

--- client/features/consultation/Styled/Proposal.js
+++ client/features/consultation/Styled/Proposal.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import { intToPx } from 'Shared/helpers/styled';
-import { DefaultPadding } from 'Client/app/assets/vars/Breakpoints';
+import {
+  DefaultPadding,
+  Breakpoints,
+} from 'Client/app/assets/vars/Breakpoints';
 import { BasicColors, ShadowColors } from 'Client/app/assets/vars/Colors';
 import { FourthLevelTitleStyle } from 'Client/ui/Elements/TitleElements';
 import { MakeFonts } from 'Client/app/assets/vars/Fonts';
@@ -9,7 +12,10 @@ export const ProposalWrapperStyle = styled.aside`
   padding: ${intToPx(DefaultPadding.Mobile)};
   background-color: ${BasicColors.PureWhite};
   box-shadow: 0 1px 1px 0 ${ShadowColors.BlackZeroFiveOpacity};
-  margin: 0 0 15px;
+  margin: 15px 0;
+  @media (min-width: ${intToPx(Breakpoints.Desktop)}) {
+    margin: 0 0 15px;
+  }
 `;
 
 export const ProposalTitleStyle = styled(FourthLevelTitleStyle)`
