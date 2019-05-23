724f6cadc085f380c1cb9a1aabdf2ada227d0b06
Olivier Donze <od@make.org>
2019-05-23 16:30:38 +0200

    feat(a11y): add user profile skiplinks

---
+++ client/app/SkipLinks/Profile/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { i18n } from 'Shared/i18n';
+import { SkipLink } from 'Client/app/Styled/MainElements';
+import { UnstyledListStyle } from 'Client/ui/Elements/ListElements';
+
+export const UserProfileSkipLinks = () => {
+  return (
+    <UnstyledListStyle aria-label={i18n.t('skip_links.secondary_list')}>
+      <li>
+        <SkipLink as="a" href="#sidebar_content" tabIndex={1}>
+          {i18n.t('skip_links.sidebar_content')}
+        </SkipLink>
+      </li>
+      <li>
+        <SkipLink as="a" href="#profile_nav" tabIndex={1}>
+          {i18n.t('skip_links.secondary_nav')}
+        </SkipLink>
+      </li>
+    </UnstyledListStyle>
+  );
+};
