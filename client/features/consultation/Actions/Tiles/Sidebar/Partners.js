5a7a962e4060432df4992d86c4e92bb95fe67f12
Olivier Donze <od@make.org>
2019-05-27 17:30:02 +0200

    feat(consultation): add target blank on link

--- client/features/consultation/Actions/Tiles/Sidebar/Partners.js
+++ client/features/consultation/Actions/Tiles/Sidebar/Partners.js
@@ -6,6 +6,8 @@ import { i18n } from 'Shared/i18n';
 import { PartnersList } from 'Client/features/consultation/Partners/List';
 import { ParagraphRedLinkStyle } from 'Client/ui/Elements/LinkElements';
 import { getPartnerAnchor } from 'Shared/helpers/url';
+import { MakeThemeColors } from 'Client/app/assets/vars/Colors';
+import { SvgExternalLink } from 'Client/ui/Svg/elements';
 
 type Props = {
   questionConfiguration: QuestionConfiguration,
@@ -23,8 +25,13 @@ export const PartnersTileContent = (props: Props) => {
       <ParagraphRedLinkStyle
         href={getPartnerAnchor(aboutUrl)}
         onClick={trackMoreLink}
+        target="_blank"
       >
         {i18n.t('consultation.partners.commitment_link')}
+        <SvgExternalLink
+          aria-label={i18n.t('common.open_new_window')}
+          style={{ marginLeft: '5px', fill: MakeThemeColors.Red }}
+        />
       </ParagraphRedLinkStyle>
     </React.Fragment>
   );
