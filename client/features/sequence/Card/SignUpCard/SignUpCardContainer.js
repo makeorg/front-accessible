cc78bf65c44d6f46f31f630fd3c482709c0124b1
Amine Zaghdoudi <az@make.org>
2019-06-05 10:44:28 +0200

    refactor(sequence): use hooks push proposal card :point_right:

--- client/features/sequence/Card/SignUpCard/SignUpCardContainer.js
+++ client/features/sequence/Card/SignUpCard/SignUpCardContainer.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { type SignUpCardConfig } from 'Shared/types/card';
 import { Tracking } from 'Shared/services/Tracking';
 import { SignUpCardComponent } from './SignUpCardComponent';
@@ -8,37 +8,35 @@ import { SignUpCardComponent } from './SignUpCardComponent';
 type Props = {
   /** Object with Static properties used to configure the Sign Up Card */
   configuration: SignUpCardConfig,
-  /** Method called we pass to the next card */
-  incrementCurrentIndex: () => void,
   /** Boolean toggled when card is visible / hidden */
   isCardVisible: boolean,
+  /** Method called we pass to the next card */
+  incrementCurrentIndex: () => void,
 };
 
 /**
  * Handles Sign Up Card Business Logic
  */
-export class SignUpCardContainer extends React.Component<Props> {
-  componentDidUpdate = () => {
-    const { isCardVisible } = this.props;
+export const SignUpCardContainer = ({
+  configuration,
+  isCardVisible,
+  incrementCurrentIndex,
+}: Props) => {
+  useEffect(() => {
     if (isCardVisible) {
-      Tracking.trackDisplaySignUpCard();
+      Tracking.trackDisplayProposalPushCard();
     }
-  };
+  }, [isCardVisible]);
 
-  skipSignUpCard = () => {
-    const { incrementCurrentIndex } = this.props;
+  const skipSignUpCard = () => {
     Tracking.trackSkipSignUpCard();
     incrementCurrentIndex();
   };
 
-  render() {
-    const { configuration } = this.props;
-
-    return (
-      <SignUpCardComponent
-        configuration={configuration}
-        skipSignUpCard={this.skipSignUpCard}
-      />
-    );
-  }
-}
+  return (
+    <SignUpCardComponent
+      configuration={configuration}
+      skipSignUpCard={skipSignUpCard}
+    />
+  );
+};
