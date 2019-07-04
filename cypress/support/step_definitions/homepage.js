8f28e5b5624fb920abd5ff3ed01f1114404101f8
Amine Zaghdoudi <az@make.org>
2019-07-04 16:48:45 +0200

    chore(transverse): add hompeage e2e tests

--- cypress/support/step_definitions/homepage.js
+++ cypress/support/step_definitions/homepage.js
@@ -1,3 +1,32 @@
+import { pages } from './shared';
+
+then('I should be redirect to {string}', (targetPage) => {
+  if (!pages[targetPage]) {
+    throw Error(`You should define "${targetPage}" path`);
+  }
+
+  cy.url().should('include', pages[targetPage]);
+});
 then(`I see {string} in the title`, (title) => {
   cy.get('title').should('have.text', title);
-})
+});
+then(`I see {string} as the title of the first section`, (title) => {
+  cy.get('#featured_title').should('have.text', title);
+});
+then(`I see {string} as the title of the second section`, (title) => {
+  cy.get('#current_consultations_title').should('have.text', title);
+});
+then('I see the first corporate bloc', () => {
+  cy.get('#corporate_title').should('have.text', 'la politique ne suffit plus, faisons bouger les lignes ensemble');
+});
+then('I see the second corporate bloc', () => {
+  cy.get('#who_are_we_title').should('have.text', 'qui sommes-nous ?');
+});
+then('I see a list of consultation', () => {
+  cy.get('#business_consultations').find('ul>li').should(($lis) => {
+    expect($lis.length).to.be.at.least(3);
+  });
+});
+then(`I see {string} as the title of the {string} section`, (title, sectionName) => {
+  cy.get(`#${sectionName}_proposals_title`).contains(title);
+});
--- cypress/support/step_definitions/shared.js
+++ cypress/support/step_definitions/shared.js
@@ -1,8 +1,16 @@
-export const url = 'http://localhost:3000';
 
-given('I open home page', () => {
-  cy.visit(url);
-})
+export const pages = {
+  'homepage': '/',
+  'france homepage': '/FR-fr',
+};
+
+given('I go to {string}', (targetPage) => {
+  if (!pages[targetPage]) {
+    throw Error(`You should define "${targetPage}" path`);
+  }
+
+  cy.visit(pages[targetPage]);
+});
 
 then('The mouse is focused in {string} field', (field) => {
   cy.focused().should('have.attr', 'id').and('eq', field);
