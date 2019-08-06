982f3209aefd50c2e545588a04c5385a0e4928ad
Amine Zaghdoudi <az@make.org>
2019-08-06 17:21:52 +0200

    chore(transverse): add mock for proposals, tags and questions

--- apiMocks/server.js
+++ apiMocks/server.js
@@ -8,8 +8,23 @@ const middlewares = jsonServer.defaults();
 
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
-
+server.use(
+  jsonServer.rewriter({
+    '/:resource\\?:params': '/:resource/data?:params',
+    '/:resource': '/:resource/data',
+    '/questions/:questionSlug/details': '/questions/data?slug=:questionSlug',
+  })
+);
 server.use('/views', jsonServer.router(path.join(__dirname, 'db/views.json')));
+server.use('/tags', jsonServer.router(path.join(__dirname, 'db/tags.json')));
+server.use(
+  '/proposals',
+  jsonServer.router(path.join(__dirname, 'db/proposals.json'))
+);
+server.use(
+  '/questions',
+  jsonServer.router(path.join(__dirname, 'db/questions.json'))
+);
 server.use('/user', userRouter);
 server.use('/proposals', proposalsRouter);
 server.use('/tracking/front', (req, res) => {
