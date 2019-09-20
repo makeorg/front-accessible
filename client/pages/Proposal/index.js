9bc55e84563db2ce715cd418d23c1753588bfca9
Olivier Donze <od@make.org>
2019-09-20 11:07:14 +0200

    refactor(proposal): getQuestionDetails in proposal page

--- client/pages/Proposal/index.js
+++ client/pages/Proposal/index.js
@@ -1,82 +1,76 @@
-/* @flow */
-
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+// @flow
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { i18n } from 'Shared/i18n';
 import { type Proposal as TypeProposal } from 'Shared/types/proposal';
-import { type QuestionConfiguration as TypeQuestionConfiguration } from 'Shared/types/sequence';
-import { fetchProposalData } from 'Shared/store/actions/proposal';
-import { fetchQuestionConfigurationData } from 'Shared/store/actions/sequence';
+import { type Question as TypeQuestion } from 'Shared/types/question';
 import { MetaTags } from 'Client/app/MetaTags';
 import { type match as TypeMatch } from 'react-router';
 import { MiddlePageWrapperStyle } from 'Client/app/Styled/MainElements';
-import { selectCurrentQuestionConfiguration } from 'Shared/store/selectors/questions.selector';
+import { Spinner } from 'Client/ui/Elements/Loading/Spinner';
+import { ProposalSkipLinks } from 'Client/app/SkipLinks/Proposal';
+import { SingleProposalCard } from 'Client/features/proposal/SingleProposalCard';
 import { apiClient } from 'Shared/api/ApiService/ApiService.client';
-import { ProposalPageContentLoader } from './ContentLoader';
+import { ProposalApiService } from 'Shared/api/ProposalApiService';
+import { QuestionApiService } from 'Shared/api/QuestionApiService';
+import { SingleProposalSharingComponent } from 'Client/features/proposal/SingleProposalCard/Sharing';
 
 type Props = {
-  proposal: TypeProposal,
-  questionConfiguration: TypeQuestionConfiguration,
-  fetchProposal: (proposalId: string) => void,
   match: TypeMatch,
 };
 
-const ProposalPageContainer = (props: Props) => {
-  const { match, proposal, fetchProposal, questionConfiguration } = props;
+const ProposalPage = (props: Props) => {
+  const { match } = props;
   const { proposalId } = match.params;
-  useEffect(() => {
-    if (!proposal) {
-      fetchProposal(proposalId);
-    }
-  }, [fetchProposal, proposal, proposalId]);
+  const [proposal, setProposal] = useState<?TypeProposal>(undefined);
+  const [question, setQuestion] = useState<?TypeQuestion>(undefined);
 
-  if (!questionConfiguration) {
-    return <MetaTags description={i18n.t('meta.proposal.description')} />;
+  useEffect(() => {
+    ProposalApiService.getProposal(proposalId).then(response => {
+      setProposal(response);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (proposal) {
+      QuestionApiService.getDetail(proposal.question.questionId).then(
+        response => {
+          setQuestion(response);
+          apiClient.questionId = response.questionId;
+          apiClient.operationId = response.operationId;
+        }
+      );
+    }
+  }, [proposal]);
+
+  if (!question) {
+    return (
+      <MiddlePageWrapperStyle>
+        <Spinner />
+      </MiddlePageWrapperStyle>
+    );
   }
 
   return (
-    <ThemeProvider theme={questionConfiguration.theme}>
+    <ThemeProvider theme={question.theme}>
       <MiddlePageWrapperStyle>
-        <MetaTags
-          title={i18n.t('meta.proposal.title', { proposal: proposal.content })}
-          description={i18n.t('meta.proposal.description')}
-        />
+        <ProposalSkipLinks />
         {proposal && (
-          <ProposalPageContentLoader
-            proposal={proposal}
-            questionConfiguration={questionConfiguration}
-          />
+          <React.Fragment>
+            <MetaTags
+              title={i18n.t('meta.proposal.title', {
+                proposal: proposal.content,
+              })}
+              description={i18n.t('meta.proposal.description')}
+            />
+            <SingleProposalCard proposal={proposal} />
+          </React.Fragment>
         )}
+        <SingleProposalSharingComponent />
       </MiddlePageWrapperStyle>
     </ThemeProvider>
   );
 };
-
-const mapStateToProps = state => {
-  const { data } = state.proposal;
-
-  return {
-    proposal: data,
-    questionConfiguration: selectCurrentQuestionConfiguration(state),
-  };
-};
-
-const mapDispatchToProps = dispatch => ({
-  fetchProposal: (proposalId: string) => {
-    dispatch(fetchProposalData(proposalId)).then((proposal: TypeProposal) => {
-      const { question } = proposal;
-      apiClient.questionId = question.questionId;
-      apiClient.operationId = question.operationId;
-      dispatch(fetchQuestionConfigurationData(question.slug));
-    });
-  },
-});
-
-export const ProposalPage = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ProposalPageContainer);
 
 // default export needed for loadable component
 export default ProposalPage; // eslint-disable-line import/no-default-export
