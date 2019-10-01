883fe1a45003b6fab7479438dd1e441ea362869c
Olivier Donze <od@make.org>
2019-10-01 14:29:06 +0200

    feat(consultation): add toLocaleString for key results :pencil:

--- client/features/consultation/Results/KeyFigures/index.js
+++ client/features/consultation/Results/KeyFigures/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { i18n } from 'Shared/i18n';
 import {
   KeyFiguresListStyle,
@@ -16,23 +17,26 @@ type Props = {
 };
 
 export const KeyFigures = ({ results, themeColor }: Props) => {
+  const language = useSelector(state => state.appConfig.language);
+  const country = useSelector(state => state.appConfig.country);
+
   return (
     <KeyFiguresListStyle>
       <KeyFiguresListItemStyle>
         <KeyFiguresCountStyle fontColor={themeColor}>
-          {results.participants}
+          {results.participants.toLocaleString(`${language}-${country}`)}
         </KeyFiguresCountStyle>
         {i18n.t('consultation.results.key_figures.participants')}
       </KeyFiguresListItemStyle>
       <KeyFiguresListItemStyle>
         <KeyFiguresCountStyle fontColor={themeColor}>
-          {results.proposals}
+          {results.proposals.toLocaleString(`${language}-${country}`)}
         </KeyFiguresCountStyle>
         {i18n.t('consultation.results.key_figures.proposals')}
       </KeyFiguresListItemStyle>
       <KeyFiguresListItemStyle>
         <KeyFiguresCountStyle fontColor={themeColor}>
-          {results.votes}
+          {results.votes.toLocaleString(`${language}-${country}`)}
         </KeyFiguresCountStyle>
         {i18n.t('consultation.results.key_figures.votes')}
       </KeyFiguresListItemStyle>
