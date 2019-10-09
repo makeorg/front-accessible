e27dcf40aa366786376f33d94df7577bd4be2934
Olivier Donze <od@make.org>
2019-10-09 02:12:52 +0200

    feat(tracking): consultation & results page tracking

--- client/features/consultation/TabsContent/Panel/Results.js
+++ client/features/consultation/TabsContent/Panel/Results.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useEffect } from 'react';
 import { type QuestionConfiguration as TypeQuestionConfiguration } from 'Shared/types/sequence';
 import {
   type Question as TypeQuestion,
@@ -17,6 +17,7 @@ import {
   SvgLightning,
   SvgThumbsUp,
 } from 'Client/ui/Svg/elements';
+import { trackDisplayConsultation } from 'Shared/services/Tracking';
 import { ConsultationPannelSidebar } from '../../Sidebar/ConsultationPannel';
 import { ResultsContext } from '../../Results/Context';
 import { ResultsIconsStyle } from '../../Results/Styled';
@@ -34,59 +35,65 @@ export const ResultsPannel = ({
   questionResults,
   questionConfiguration,
   question,
-}: Props) => (
-  <React.Fragment>
-    <MetaTags
-      title={i18n.t('meta.consultation.results.title', {
-        question: question.wording.question,
-      })}
-    />
-    <ConsultationPannelSidebar
-      question={question}
-      questionConfiguration={questionConfiguration}
-    />
-    <ConsultationPageContentStyle id="main">
-      <HiddenItemStyle as="h2">
-        {i18n.t('consultation.results.title', {
+}: Props) => {
+  useEffect(() => {
+    trackDisplayConsultation('results');
+  }, []);
+
+  return (
+    <React.Fragment>
+      <MetaTags
+        title={i18n.t('meta.consultation.results.title', {
           question: question.wording.question,
         })}
-      </HiddenItemStyle>
-      <TileWithTitle
-        title={i18n.t('consultation.results.context')}
-        icon={<SvgInfos aria-hidden style={ResultsIconsStyle} />}
-      >
-        <ResultsContext
-          context={questionResults.context}
-          aboutUrl={question.aboutUrl}
-        />
-      </TileWithTitle>
-      <TileWithTitle
-        title={i18n.t('consultation.results.key_figures.title')}
-        icon={<SvgCalculator aria-hidden style={ResultsIconsStyle} />}
-      >
-        <KeyFigures
-          results={questionResults.key_figures}
-          themeColor={questionConfiguration.theme.color}
-        />
-      </TileWithTitle>
-      <TileWithTitle
-        title={i18n.t('consultation.results.top_ideas.title')}
-        icon={<SvgLightBulb aria-hidden style={ResultsIconsStyle} />}
-      >
-        <TopIdeas topIdeas={questionResults.top_ideas} />
-      </TileWithTitle>
-      <TileWithTitle
-        title={i18n.t('consultation.results.proposals.controversials')}
-        icon={<SvgLightning aria-hidden style={ResultsIconsStyle} />}
-      >
-        <ProposalsResults proposals={questionResults.controversials} />
-      </TileWithTitle>
-      <TileWithTitle
-        title={i18n.t('consultation.results.proposals.rejected')}
-        icon={<SvgThumbsUp aria-hidden style={ResultsIconsStyle} />}
-      >
-        <ProposalsResults proposals={questionResults.rejected} isRejected />
-      </TileWithTitle>
-    </ConsultationPageContentStyle>
-  </React.Fragment>
-);
+      />
+      <ConsultationPannelSidebar
+        question={question}
+        questionConfiguration={questionConfiguration}
+      />
+      <ConsultationPageContentStyle id="main">
+        <HiddenItemStyle as="h2">
+          {i18n.t('consultation.results.title', {
+            question: question.wording.question,
+          })}
+        </HiddenItemStyle>
+        <TileWithTitle
+          title={i18n.t('consultation.results.context')}
+          icon={<SvgInfos aria-hidden style={ResultsIconsStyle} />}
+        >
+          <ResultsContext
+            context={questionResults.context}
+            aboutUrl={question.aboutUrl}
+          />
+        </TileWithTitle>
+        <TileWithTitle
+          title={i18n.t('consultation.results.key_figures.title')}
+          icon={<SvgCalculator aria-hidden style={ResultsIconsStyle} />}
+        >
+          <KeyFigures
+            results={questionResults.key_figures}
+            themeColor={questionConfiguration.theme.color}
+          />
+        </TileWithTitle>
+        <TileWithTitle
+          title={i18n.t('consultation.results.top_ideas.title')}
+          icon={<SvgLightBulb aria-hidden style={ResultsIconsStyle} />}
+        >
+          <TopIdeas topIdeas={questionResults.top_ideas} />
+        </TileWithTitle>
+        <TileWithTitle
+          title={i18n.t('consultation.results.proposals.controversials')}
+          icon={<SvgLightning aria-hidden style={ResultsIconsStyle} />}
+        >
+          <ProposalsResults proposals={questionResults.controversials} />
+        </TileWithTitle>
+        <TileWithTitle
+          title={i18n.t('consultation.results.proposals.rejected')}
+          icon={<SvgThumbsUp aria-hidden style={ResultsIconsStyle} />}
+        >
+          <ProposalsResults proposals={questionResults.rejected} isRejected />
+        </TileWithTitle>
+      </ConsultationPageContentStyle>
+    </React.Fragment>
+  );
+};
