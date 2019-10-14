08729719fc54c2e8ea9c81e1f79bd3c292c79952
Olivier Donze <od@make.org>
2019-10-14 15:08:06 +0200

    feat(consultation): handle question if logo is missing :art:

--- client/features/consultation/IntroBanner/index.js
+++ client/features/consultation/IntroBanner/index.js
@@ -1,2 +1,41 @@
 // @flow
-export { IntroBannerComponent as IntroBanner } from './IntroBannerComponent';
+import React from 'react';
+import { i18n } from 'Shared/i18n';
+import { isGreatCause } from 'Shared/helpers/question';
+import { type Question } from 'Shared/types/question';
+import { type QuestionConfiguration } from 'Shared/types/sequence';
+import {
+  IntroWrapperStyle,
+  IntroLabelStyle,
+  IntroBannerTitleStyle,
+} from '../Styled/IntroBanner';
+
+type Props = {
+  question: Question,
+  questionConfiguration: QuestionConfiguration,
+};
+
+export const IntroBanner = ({ question, questionConfiguration }: Props) => {
+  const { wording } = question;
+  const { theme, consultation } = questionConfiguration;
+  return (
+    <IntroWrapperStyle
+      as="header"
+      gradientStart={theme.gradientStart}
+      gradientEnd={theme.gradientEnd}
+      backgroundcolor={theme.gradientStart}
+      id="intro"
+    >
+      {isGreatCause(question.operationKind) && (
+        <IntroLabelStyle>{i18n.t('consultation.header.label')}</IntroLabelStyle>
+      )}
+      {consultation.logo ? (
+        <h2>
+          <img src={consultation.logo} alt={wording.question} />
+        </h2>
+      ) : (
+        <IntroBannerTitleStyle>{wording.question}</IntroBannerTitleStyle>
+      )}
+    </IntroWrapperStyle>
+  );
+};
