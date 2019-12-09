c322ed485b2818a56801470a4d86cdb4886a5966
fali.rasolo <falihery.rasolonirainy@make.org>
2019-12-09 11:09:39 +0100

    style(consultation): update tags style

--- client/ui/Elements/Tag/Styled/index.js
+++ client/ui/Elements/Tag/Styled/index.js
@@ -1,50 +1,13 @@
 import styled from 'styled-components';
-import {
-  BasicColors,
-  TextColors,
-  BackgroundColors,
-  MakeThemeColors,
-} from 'Client/app/assets/vars/Colors';
+import { BasicColors, MakeThemeColors } from 'Client/app/assets/vars/Colors';
 import { intToPx } from 'Shared/helpers/styled';
 import { Breakpoints } from 'Client/app/assets/vars/Breakpoints';
+import { ParagraphStyle } from 'Client/ui/Elements/ParagraphElements';
 
 const DefaultTagStyle = `
-  position: relative;
-  padding: 0 10px 0 12px;
-  color: ${TextColors.MediumGrey};
-  background-color: ${BackgroundColors.ExtraLightGrey};
-  text-decoration: none;
-  line-height: 24px;
-  font-size: 14px;
-  border: none;
-  margin-left: 10px;
   margin-right: 10px;
   margin-top: 10px;
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: -10px;
-    width: 0;
-    height: 0;
-    border-color: transparent ${
-      BackgroundColors.ExtraLightGrey
-    } transparent transparent;
-    border-style: solid;
-    border-width: 12px 10px 12px 0;
-  }
-  &:after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 0;
-    float: left;
-    width: 6px;
-    height: 6px;
-    transform: translateY(-50%);
-    border-radius: 3px;
-    background-color: ${TextColors.MediumGrey};
-  }
+  text-decoration: underline;
 `;
 
 const SelectedTagStyle = `
@@ -59,7 +22,7 @@ const SelectedTagStyle = `
   }
 `;
 
-export const TagStyle = styled.span`
+export const TagStyle = styled(ParagraphStyle)`
   ${DefaultTagStyle};
 `;
 
