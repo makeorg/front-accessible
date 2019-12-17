4618b1c507e4c6f9f1cf3cfd0b08b7f05bdaac9b
fali.rasolo <falihery.rasolonirainy@make.org>
2019-12-17 18:29:18 +0100

    feat(transverse): display medias logos & founders

---
+++ client/features/consultation/Sidebar/Founders/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { ParagraphStyle } from 'Client/ui/Elements/ParagraphElements';
+
+export const FoundersTitleStyle = styled(ParagraphStyle)`
+  margin: 20px 0;
+`;
