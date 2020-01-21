c37dd7899fa527f77b53f98162e1d15f1014d3fb
Philippe de Araujo <pa@make.org>
2020-01-21 16:54:20 +0100

    chore(transverse): update footer links
    close #mp-783

--- shared/constants/url.js
+++ shared/constants/url.js
@@ -1,5 +1,6 @@
 export const WHOAREWE_FR_LINK = 'https://about.make.org/qui-sommes-nous';
 export const JOBS_LINK = 'https://about.make.org/jobs';
-export const PRESS_LINK = 'https://about.make.org/category/presse';
 export const MODERATION_CHARTER_LINK = 'https://about.make.org/moderation';
 export const ABOUT_MAKE_LINK = 'https://about.make.org/';
+export const DOTATION_FUNDS_LINK = 'https://foundation.make.org';
+export const NEWS_LINK = 'https://about.make.org';
