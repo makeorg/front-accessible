29b4298f8758bf9b321c3092958a37c291e16579
Olivier Donze <od@make.org>
2020-07-28 15:07:36 +0200

    Revert "feat(transverse): add strict-transport-security"
    This reverts commit a79cc0ece7b55ba9756a4767ff2e586934445458.

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,9 +1,5 @@
 export const headersResponseMiddleware = (req, res, next) => {
   res.setHeader('Server', 'Express');
-  res.setHeader(
-    'Strict-Transport-Security',
-    'max-age=31536000; includeSubDomains; preload;'
-  );
 
   return next();
 };
