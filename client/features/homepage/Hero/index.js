23c2e48550c18e1495fddb0ccd4d8442810edd39
Olivier Donze <od@make.org>
2020-08-06 10:44:42 +0200

    fix(homepage): hero various fixes

--- client/features/homepage/Hero/index.js
+++ client/features/homepage/Hero/index.js
@@ -16,43 +16,46 @@ import {
   BlackArrowDownIcon,
   HeroContentStyle,
   HeroInnerContentStyle,
+  HeroWrapperStyle,
 } from './style';
 
 export const Hero = () => {
   const isDesktop = useDesktop();
 
   return (
-    <HeroContentStyle as="section" aria-labelledby="hero-title">
-      <HeroInnerContentStyle>
-        <HeroTitleStyle id="hero-title" data-cy-container="hero-title">
-          {i18n.t('homepage.hero.title')}
-        </HeroTitleStyle>
-        {!isDesktop && <HeroPictures />}
-        <HeroDescriptionStyle>
-          {i18n.t('homepage.hero.description')}
-        </HeroDescriptionStyle>
-        <ColumnToRowToColumnStyle>
-          <HeroRedButtonStyle
-            as="a"
-            href="#current_questions"
-            onClick={() => trackClickHomepageParticipate()}
-            data-cy-link="participate-consultations"
-          >
-            {i18n.t('homepage.hero.participate')}
-            <WhiteArrowDownIcon aria-hidden />
-          </HeroRedButtonStyle>
-          <HeroTransparentButtonStyle
-            as="a"
-            href="#featured_questions"
-            onClick={() => trackClickHomepageDiscover()}
-            data-cy-link="discover-great-causes"
-          >
-            {i18n.t('homepage.hero.discover')}
-            <BlackArrowDownIcon aria-hidden />
-          </HeroTransparentButtonStyle>
-        </ColumnToRowToColumnStyle>
-      </HeroInnerContentStyle>
-      {isDesktop && <HeroPictures />}
-    </HeroContentStyle>
+    <HeroWrapperStyle as="section" aria-labelledby="hero-title">
+      <HeroContentStyle>
+        <HeroInnerContentStyle>
+          <HeroTitleStyle id="hero-title" data-cy-container="hero-title">
+            {i18n.t('homepage.hero.title')}
+          </HeroTitleStyle>
+          {!isDesktop && <HeroPictures />}
+          <HeroDescriptionStyle>
+            {i18n.t('homepage.hero.description')}
+          </HeroDescriptionStyle>
+          <ColumnToRowToColumnStyle>
+            <HeroRedButtonStyle
+              as="a"
+              href="#current_questions"
+              onClick={() => trackClickHomepageParticipate()}
+              data-cy-link="participate-consultations"
+            >
+              {i18n.t('homepage.hero.participate')}
+              <WhiteArrowDownIcon aria-hidden />
+            </HeroRedButtonStyle>
+            <HeroTransparentButtonStyle
+              as="a"
+              href="#featured_questions"
+              onClick={() => trackClickHomepageDiscover()}
+              data-cy-link="discover-great-causes"
+            >
+              {i18n.t('homepage.hero.discover')}
+              <BlackArrowDownIcon aria-hidden />
+            </HeroTransparentButtonStyle>
+          </ColumnToRowToColumnStyle>
+        </HeroInnerContentStyle>
+        {isDesktop && <HeroPictures />}
+      </HeroContentStyle>
+    </HeroWrapperStyle>
   );
 };
