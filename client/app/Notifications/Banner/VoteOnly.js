2535654882e93f03b48815b63d2b211961052e3f
Olivier Donze <od@make.org>
2021-02-02 10:57:23 +0100

    feat(consultation): vote only banner evolution

--- client/app/Notifications/Banner/VoteOnly.js
+++ client/app/Notifications/Banner/VoteOnly.js
@@ -1,17 +1,43 @@
 // @flow
-import React from 'react';
+import React, { useEffect } from 'react';
 import { i18n } from 'Shared/i18n';
-
+import { type StateRoot } from 'Shared/store/types';
+import { type QuestionType } from 'Shared/types/question';
 import {
   VoteOnlyMessageStyle,
   VoteOnlyButtonStyle,
 } from 'Client/ui/Elements/Notifications/Banner/style';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCurrentQuestion } from 'Shared/store/selectors/questions.selector';
+import { clearNotificationBanner } from 'Shared/store/actions/notifications';
 
-export const VoteOnlyMessage = ({ close }) => (
-  <VoteOnlyMessageStyle>
-    {i18n.t('common.notifications.vote_only.message')}
-    <VoteOnlyButtonStyle aria-expanded="false" onClick={close}>
-      {i18n.t('common.notifications.vote_only.button')}
-    </VoteOnlyButtonStyle>
-  </VoteOnlyMessageStyle>
-);
+type Props = {
+  close: () => {},
+};
+
+export const VoteOnlyMessage = ({ close }: Props) => {
+  const question: QuestionType = useSelector((state: StateRoot) =>
+    selectCurrentQuestion(state)
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!question) {
+      dispatch(clearNotificationBanner());
+    }
+  }, [question, dispatch]);
+
+  if (!question) {
+    return null;
+  }
+  return (
+    <VoteOnlyMessageStyle>
+      {i18n.t('common.notifications.vote_only.message', {
+        title: question.question,
+      })}
+      <VoteOnlyButtonStyle aria-expanded="false" onClick={close}>
+        {i18n.t('common.notifications.vote_only.button')}
+      </VoteOnlyButtonStyle>
+    </VoteOnlyMessageStyle>
+  );
+};
