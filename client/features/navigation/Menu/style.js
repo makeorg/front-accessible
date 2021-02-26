b57319ceb27122860583a7351373f33e5ecc2ea3
Tudare <aurelie.tudare@make.org>
2021-02-26 15:28:37 +0100

    feat(transverse): burger background color

--- client/features/navigation/Menu/style.js
+++ client/features/navigation/Menu/style.js
@@ -41,7 +41,7 @@ export const MenuPanelStyle = styled.div`
   z-index: 10;
   width: 100vw;
   height: 100vh;
-  background-color: ${color.white};
+  background-color: ${color.greyLighter};
   box-shadow: 0px 0px 20px ${ShadowColors.BlackZeroEightOpacity};
   transition: 0.5s ease-in left;
   &.expanded {
