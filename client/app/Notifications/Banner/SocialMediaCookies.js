fb6901d7b56d3443f416eca3f510a255677d67f8
Olivier Donze <od@make.org>
2021-04-20 15:01:45 +0200

    feat(transverse): social sharing notification adjusment

--- client/app/Notifications/Banner/SocialMediaCookies.js
+++ client/app/Notifications/Banner/SocialMediaCookies.js
@@ -1,19 +1,7 @@
 // @flow
-import { WhiteLink } from 'Client/ui/Elements/Notifications/Banner/style';
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { i18n } from 'Shared/i18n';
-import { modalCloseCookies } from 'Shared/store/actions/modal';
 
-export const SocialMediaCookiesMessage = () => {
-  const dispatch = useDispatch();
-  return (
-    <>
-      {i18n.t('common.notifications.cookies.social_media')}
-      <WhiteLink to="#" onClick={() => dispatch(modalCloseCookies())}>
-        {i18n.t('common.notifications.cookies.cookie_page')}
-      </WhiteLink>
-      .
-    </>
-  );
-};
+export const SocialMediaCookiesMessage = () => (
+  <>{i18n.t('common.notifications.cookies.social_media')}</>
+);
