a487d0addd83f20e4728d9b379cc8242e6efce5a
Olivier Donze <od@make.org>
2021-07-01 16:00:52 +0200

    feat(transverse): homepage & meta images for de country

--- client/features/homepage/Hero/Pictures.js
+++ client/features/homepage/Hero/Pictures.js
@@ -6,14 +6,20 @@ import { useTablet } from 'Client/hooks/useMedia';
 import { HeroPicturesStyle } from './style';
 
 export const HeroPictures = () => {
-  const { device } = useSelector((state: StateRoot) => state.appConfig);
+  const { device, country } = useSelector(
+    (state: StateRoot) => state.appConfig
+  );
   const isDesktopInState = matchDesktopDevice(device);
   const isTabletViewport = useTablet();
+  const imageCommonPath = 'https://assets.make.org/assets/home';
 
-  const HeroDesktop =
-    'https://assets.make.org/assets/home/hero-desktop-170720.png';
-  const HeroSmallDevices =
-    'https://assets.make.org/assets/home/hero-tablettes-170720.png';
+  let HeroDesktop = `${imageCommonPath}/hero-desktop-170720.png`;
+  let HeroSmallDevices = `${imageCommonPath}/hero-tablettes-170720.png`;
+
+  if (country === 'DE') {
+    HeroDesktop = `${imageCommonPath}/homepage_desktop_de.png`;
+    HeroSmallDevices = `${imageCommonPath}/homepage_mobile_de.png`;
+  }
 
   let HeroPictureWidth = 335;
   let HeroPictureHeight = 202;
